2a197584300848ab22b84b27291c6e5c9b4ae392
Heewon Cho <heewon6752@gmail.com>
2025-03-02 16:27:30 +0900

    Update basic content

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
-export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+export const SITE_URL = "https://zig-kr.org";
+export const SITE_TITLE = "Zig Korea";
+export const SITE_DESCRIPTION = "한국 Zig 사용자 그룹";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "admin@zig-kr.org";
